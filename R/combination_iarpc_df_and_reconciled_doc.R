373d1a8c3e1a090287381c2b35ed998dcc7b8a2b
Cohen <ecohen@ida.org>
2021-05-20 09:09:03 -0400

    adds join R script for reconciled doc

---
+++ R/combination_iarpc_df_and_reconciled_doc.R
@@ -0,0 +1,37 @@
+# Script to combine reconciled doc and lead agency
+
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(here)
+  library(readr)
+  library(readxl)
+  library(janitor)
+})
+
+data_raw <- here("data-raw/")
+data_output <- here("data-output/")
+
+condensed_file_name <- "PE Assignments Condensed_Reconciled.xlsx"
+iarpc_df_name <- "2021-05-06_iarpc_df.RDS"
+
+condensed_file <-
+  read_excel(
+    paste0(data_raw, "/", condensed_file_name)
+    ) %>%
+  clean_names()
+
+iarpc_df <-
+  read_rds(
+    paste0(data_output, "/", iarpc_df_name)
+  )
+
+# join
+joined_iarpc_reconciled_df <-
+  iarpc_df %>%
+  left_join(condensed_file, by = "task") %>%
+  select(-starts_with("is_related_to"))
+
+write_csv(
+  joined_iarpc_reconciled_df,
+  paste0(data_output, "/", Sys.Date(), "_joined_iarpc_reconciled_df.csv")
+)
